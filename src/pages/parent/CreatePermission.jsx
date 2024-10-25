2eeb6e77ab64b9792e287d25a11620cfe3e03a17
jaugustyn02 <jaugustyn@student.agh.edu.pl>
2024-10-25 10:09:05 +0200

    No to fajne warningi jako errory panie kolego

--- src/pages/parent/CreatePermission.jsx
+++ src/pages/parent/CreatePermission.jsx
@@ -1,10 +1,3 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { Navigation } from "../../components/Navigation";
-import { LoadingSpinner } from "../../components/LoadingSpinner";
-import { Link, useNavigate } from "react-router-dom";
-import { useRecoilValue } from "recoil";
-import { authState } from "../../recoil-state/auth";
 import { useParams } from "react-router-dom";
 
 // TODO add formik in task frontend-5
