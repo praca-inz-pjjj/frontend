909f6a2dcf2743f349ac9df2493fe218ac58a8a3
pwolan <49585424+pwolan@users.noreply.github.com>
2024-11-12 19:50:12 +0100

    fixed receivers links (#29)

--- src/pages/parent/tables/PickUpsTable.jsx
+++ src/pages/parent/tables/PickUpsTable.jsx
@@ -1,51 +1,64 @@
 import React, { FC, useEffect, useState } from "react";
-import DataTable, { TableProps } from "../../../components/DataTable"
+import DataTable, { TableProps } from "../../../components/DataTable";
 
 export interface PickUpData {
-    id: string;
-    receiver_name: string;
-    receiver_surname: string;
-    teacher_name: string;
-    teacher_surname: string;
-    decision: boolean;
-    date: string;
-    child: string;
-    child_name: string;
-    child_surname: string;
-    receiver: string;
-    teacher: string;
+  id: string;
+  receiver_name: string;
+  receiver_surname: string;
+  teacher_name: string;
+  teacher_surname: string;
+  decision: boolean;
+  date: string;
+  child: string;
+  child_name: string;
+  child_surname: string;
+  receiver: string;
+  teacher: string;
 }
 
 export interface PickUpsTableProps extends TableProps {
-    pick_ups_data: PickUpData[],
+  pick_ups_data: PickUpData[];
 }
 
-export const PickUpsTable: FC<PickUpsTableProps> = ({ 
-  title, 
+export const PickUpsTable: FC<PickUpsTableProps> = ({
+  title,
   pick_ups_data,
-  no_data_message
+  no_data_message,
 }) => {
-    const [data_rows, setDataRows] = useState([])
-    const labels = ["#", "Dziecko", "Odbierający", "Nauczyciel", "Decyzja", "Data"]
+  const [data_rows, setDataRows] = useState([]);
+  const labels = [
+    "#",
+    "Dziecko",
+    "Odbierający",
+    "Nauczyciel",
+    "Decyzja",
+    "Data",
+  ];
 
-    useEffect(()=>{
-      const pick_ups_data_rows: Array[never] = pick_ups_data?.map((row, index)=>[
-        index+1,
+  useEffect(() => {
+    const pick_ups_data_rows: Array[never] = pick_ups_data?.map(
+      (row, index) => [
+        index + 1,
         `${row.child_name} ${row.child_surname}`,
         `${row.receiver_name} ${row.receiver_surname}`,
         `${row.teacher_name} ${row.teacher_surname}`,
-        row.decision ? "Odbiór" : "Brak odbioru",
-        row.date
-      ])
-      setDataRows(pick_ups_data_rows)
-    }, [pick_ups_data])
-    
-    return (
-      <DataTable 
-        title={title}
-        no_data_message={no_data_message}
-        labels={labels}
-        data_rows={data_rows}
-        />
-    )
+        row.decision ? (
+          <span className="text-green-500">Odbiór</span>
+        ) : (
+          <span className="text-red-500">Brak odbioru</span>
+        ),
+        new Date(row.date).toLocaleString(),
+      ]
+    );
+    setDataRows(pick_ups_data_rows);
+  }, [pick_ups_data]);
+
+  return (
+    <DataTable
+      title={title}
+      no_data_message={no_data_message}
+      labels={labels}
+      data_rows={data_rows}
+    />
+  );
 };
--- src/pages/parent/tables/ReceiversTable.jsx
+++ src/pages/parent/tables/ReceiversTable.jsx
@@ -1,60 +1,67 @@
 // ChildrenTable.jsx
 import React, { FC, useEffect, useState } from "react";
-import DataTable, { TableProps } from "../../../components/DataTable"
+import DataTable, { TableProps } from "../../../components/DataTable";
 import BlueLinkButton from "../../../components/buttons/BlueLinkButton";
 import { Link } from "react-router-dom";
 
-
 interface ReceiversData {
-  receiver_id: String,
-  receiver_name: String,
-  child_name: String,
-  parent_name: String,
-  date: String,
-  signature: Boolean,
-  is_parent: Boolean
+  receiver_id: String;
+  receiver_name: String;
+  child_name: String;
+  parent_name: String;
+  date: String;
+  signature: Boolean;
+  is_parent: Boolean;
 }
 
 export interface ReceiversTableProps extends TableProps {
-  receivers_data: ReceiversData[],
+  receivers_data: ReceiversData[];
 }
 
-export const ReceiversTable: FC<ReceiversTableProps> = ({ 
-    receivers_data,
-    ...props
-  }) => {
-  const [data_rows, setDataRows] = useState([])
-  const labels = ["#", "Odbierający", "Odbierany", "Dodany przez", "Data dodania", "Historia odbiorów"]
+export const ReceiversTable: FC<ReceiversTableProps> = ({
+  receivers_data,
+  ...props
+}) => {
+  const [data_rows, setDataRows] = useState([]);
+  const labels = [
+    "#",
+    "Odbierający",
+    "Odbierany",
+    "Dodany przez",
+    "Data dodania",
+    "Historia odbiorów",
+  ];
 
   useEffect(() => {
     const permitted_user_data_rows = receivers_data
       ?.filter(({ is_parent }) => !is_parent)
       .map((data, index) => [
         index + 1,
-        <Link to={`/parent/receiver/${data.receiver_id}`} key={data.receiver_id}>
+        <Link
+          to={`/parent/receiver/${data.receiver_id}?child=${data.child}`}
+          key={data.receiver_id}
+        >
           {data.receiver_name}
         </Link>,
         data.child_name,
         data.parent_name,
         data.date,
         <BlueLinkButton
-        to={`/parent/receiver/${data.receiver_id}?child=${data.child}`}
-        text={"Historia"}
-        />
+          to={`/parent/receiver/${data.receiver_id}?child=${data.child}`}
+          text={"Historia"}
+        />,
       ]);
-  
+
     setDataRows(permitted_user_data_rows);
   }, [receivers_data]);
 
   return (
-    <DataTable 
+    <DataTable
       title={props?.title}
       no_data_message={props?.no_data_message}
       labels={labels}
       data_rows={data_rows}
       buttons={props?.buttons}
-      />
-  )
+    />
+  );
 };
-
-
