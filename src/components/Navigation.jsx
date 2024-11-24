ef820f9524657762740f3d78284717065587cae4
Jan Augustyn <jaugustyn@student.agh.edu.pl>
2024-11-24 17:04:52 +0100

    New navigation, tab logo icon (#37)
    * New navigation, tab logo icon
    
    * improved logo and icon, fixed ratio diff in different browsers, small clean up
    
    * login button without special styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import logo from '../resources/logo/logo.png';
 
 export function Navigation() {
   const [isAuth, setIsAuth] = useState(false);
@@ -19,25 +20,32 @@ export function Navigation() {
     }
   }, []);
 
-  // Definiowanie klas bazowych
   const baseLinkClass = "block py-2 px-3 rounded";
-  const defaultTextClass = "text-white hover:bg-gray-700";
-  const authTeacherClass = "bg-blue-600 text-white";
-  const authParentClass = "bg-green-600 text-white";
+  const defaultTextClass = "text-gray-800 hover:bg-gray-200 font-semibold";
+  const authTeacherClass = "bg-blue-100 text-blue-800 hover:bg-blue-200 font-semibold";
+  const authParentClass = "bg-green-100 text-green-800 hover:bg-green-200 font-semibold";
 
-  // Definiowanie klas opartych na stanie
   const teacherLinkClass = `${baseLinkClass} ${isAuth && isTeacher ? authTeacherClass : defaultTextClass}`;
   const parentLinkClass = `${baseLinkClass} ${isAuth && isParent ? authParentClass : defaultTextClass}`;
 
   return (
-    <nav className="bg-gray-800">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <button
-          onClick={() => navigate(-1)}
-          className="text-white px-4 py-2 hover:bg-gray-700 rounded"
-        >
-          Powrót
-        </button>
+    <nav className="bg-white border-b border-gray-200 shadow-sm">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto pr-4">
+        <div className="flex items-start space-x-4 md:space-x-8 items-center">
+          <Link className="hover:bg-gray-200 p-4" to="/">
+            <img
+              src={logo}
+              alt="SafeKid Logo"
+              className="h-8"
+            />
+          </Link>
+          <button
+            onClick={() => navigate(-1)}
+            className="text-gray-800 px-4 py-2 hover:bg-gray-200 rounded font-semibold"
+          >
+            Powrót
+          </button>
+        </div>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="flex flex-col p-4 md:flex-row md:space-x-8 md:p-0">
             <li>
