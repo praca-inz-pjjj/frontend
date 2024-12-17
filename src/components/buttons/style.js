9eccdbbcfcf69b8c605ab83d02882d05cf35f7a9
jaugustyn02 <jaugustyn@student.agh.edu.pl>
2024-12-17 01:34:43 +0100

    another buttons update

--- src/components/buttons/style.js
+++ src/components/buttons/style.js
@@ -1,14 +1,28 @@
 export const BTN_CLASSES = "inline-block px-4 py-[4px] rounded-full focus:outline-none font-semibold text-center text-sm transition-all duration-200 ease-in-out text-nowrap";
+export const BTN_DISABLED = "opacity-50";
 
-export const COLOR_CLASSES = {
-    blue: "bg-transparent text-blue-500 border border-blue-400 hover:bg-blue-500 hover:text-white hover:border-blue-500 focus:bg-blue-500 focus:border-blue-500 focus:text-white",
-    orange: "bg-transparent text-orange-500 border border-orange-300 hover:bg-orange-500 hover:text-white hover:border-orange-500 focus:bg-orange-500 focus:border-orange-500 focus:text-white",
-    yellow: "bg-transparent text-yellow-500 border border-yellow-400 hover:bg-yellow-500 hover:text-black hover:border-yellow-500 focus:bg-yellow-500 focus:border-yellow-500 focus:text-black",
-    green: "bg-transparent text-green-500 border border-green-400 hover:bg-green-500 hover:text-white hover:border-green-500 focus:bg-green-500 focus:border-green-500 focus:text-white",
-    red: "bg-transparent text-red-600 border border-red-400 hover:bg-red-500 hover:text-white hover:border-red-500 focus:bg-red-500 focus:border-red-500 focus:text-white",
-    transparent: "bg-transparent text-gray-500 border border-gray-300 hover:bg-orange-400 hover:text-white hover:border-orange-400 focus:bg-orange-400 focus:border-orange-400 focus:text-white",
-    outlined: "bg-transparent text-slate-700 border border-slate-300 hover:bg-slate-700 hover:text-white hover:border-slate-700 focus:bg-slate-700 focus:border-slate-700 focus:text-white",
-    primary_green: "bg-green-500 text-white border border-green-500 hover:bg-transparent hover:text-green-500 hover:border-green-500 focus:bg-transparent focus:border-green-500 focus:text-green-500",
+export const BTN_COLORS = {
+    blue: "bg-transparent text-blue-500 border border-blue-400 focus:bg-blue-500 focus:border-blue-500 focus:text-white",
+    orange: "bg-transparent text-orange-500 border border-orange-300 focus:bg-orange-500 focus:border-orange-500 focus:text-white",
+    yellow: "bg-transparent text-yellow-500 border border-yellow-400 focus:bg-yellow-500 focus:border-yellow-500 focus:text-black",
+    green: "bg-transparent text-green-500 border border-green-400 focus:bg-green-500 focus:border-green-500 focus:text-white",
+    red: "bg-transparent text-red-500 border border-red-400 focus:bg-red-500 focus:border-red-500 focus:text-white",
+    slate: "bg-transparent text-slate-600 border border-slate-600 focus:bg-slate-600 focus:border-slate-600 focus:text-white",
+    primary_green: "bg-green-500 text-white border border-green-500 focus:bg-transparent focus:border-green-500 focus:text-green-500",
+    primary_blue: "bg-blue-500 text-white border border-blue-500 focus:bg-blue-700 focus:border-blue-700",
+};
+
+export const BTN_HOVER_CLASSES = {
+    blue: "hover:bg-blue-500 hover:text-white hover:border-blue-500",
+    orange: "hover:bg-orange-500 hover:text-white hover:border-orange-500",
+    yellow: "hover:bg-yellow-500 hover:text-black hover:border-yellow-500",
+    green: "hover:bg-green-500 hover:text-white hover:border-green-500",
+    red: "hover:bg-red-500 hover:text-white hover:border-red-500",
+    transparent: "hover:bg-orange-400 hover:text-white hover:border-orange-400",
+    outlined: "hover:bg-slate-700 hover:text-white hover:border-slate-700",
+    slate: "hover:bg-slate-600 hover:text-white hover:border-slate-600",
+    primary_green: "hover:bg-transparent hover:text-green-500 hover:border-green-500",
+    primary_blue: "hover:bg-blue-700 hover:border-blue-500",
 };
 
 export const CARD_BG_CLASSES = {
