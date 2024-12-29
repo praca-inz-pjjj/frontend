a68861f0af7857052b463c5c6338ee0c109c0251
jaugustyn02 <jaugustyn@student.agh.edu.pl>
2024-12-29 10:23:43 +0100

    footer upgrade

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,50 +1,59 @@
 import React from "react";
 
 const Footer = () => {
-    const authorClass = "text-gray-500 hover:text-gray-700 text-sm";
+    const authorClass = "text-gray-500 hover:text-gray-700 text-sm text-center";
     return (
-        <footer className="py-6 bg-sky-100">
+        <footer className="py-8 bg-sky-100">
             <div className="container mx-auto px-4 max-w-7xl">
-                <div className="flex flex-wrap items-center justify-between">
+                <div className="flex flex-col sm:flex-row items-center justify-between">
                     <a
                         href="https://github.com/praca-inz-pjjj"
-                        className="text-center text-gray-500 hover:text-gray-700 text-sm mt-4 md:mt-0"
+                        className="text-center text-gray-500 hover:text-gray-700 text-sm"
+                        target="_blank"
+                        rel="noopener noreferrer"
                     >
                         Projekt Inżynierski 2024-2025
                     </a>
-                    <div className="flex items-center space-x-4 mt-4 md:mt-0">
-                        <a
-                            href="https://github.com/pwolan"
-                            className={authorClass}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Piotr Wolanin
-                        </a>
-                        <a
-                            href="https://github.com/KubaBiz"
-                            className={authorClass}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Jakub Bizan
-                        </a>
-                        <a
-                            href="https://github.com/jkr02"
-                            className={authorClass}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Jakub Kroczek
-                        </a>
-                        <a
-                            href="https://github.com/jaugustyn02"
-                            className={authorClass}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Jan Augustyn
-                        </a>
+                    <div className="flex flex-wrap justify-center gap-2 mt-4 sm:mt-0">
+                        <div className="ml-2">
+                            <p className="text-gray-700 text-sm text-center">Autorzy:</p>
+                        </div>
+                        <div className="flex justify-center gap-4 sm:gap-2 w-full sm:w-auto">
+                            <a
+                                href="https://github.com/jaugustyn02"
+                                className={authorClass}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Jan Augustyn
+                            </a>
+                            <a
+                                href="https://github.com/KubaBiz"
+                                className={authorClass}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Jakub Bizan
+                            </a>
+                        </div>
+                        <div className="flex justify-center gap-4 sm:gap-2 w-full sm:w-auto">
+                            <a
+                                href="https://github.com/jkr02"
+                                className={authorClass}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Jakub Kroczek
+                            </a>
+                            <a
+                                href="https://github.com/pwolan"
+                                className={authorClass}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Piotr Wolanin
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
