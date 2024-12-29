3bf1736076aad3f80d9693de211f43a655a5b7d5
jaugustyn02 <jaugustyn@student.agh.edu.pl>
2024-12-29 16:07:07 +0100

    main login page upgrade

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Navigation } from "../components/Navigation";
+import { Navigation } from "../components/navigation/Navigation";
 import Body from "../components/Body";
 import ColorfulLinkButton from "../components/buttons/ColorfulLinkButton"
 
@@ -23,50 +23,48 @@ export const Login = () => {
     <Body>
       <Navigation />
       <div className="flex flex-col items-center sm:mt-16 md:mt-32">
-        <div className="bg-white p-8 sm:rounded-lg w-full max-w-5xl sm:shadow-lg">
+        <div className="bg-white p-4 sm:p-8 sm:rounded-lg w-full max-w-7xl sm:shadow-lg">
           <div className="flex flex-col sm:flex-row divide-y sm:divide-y-0 sm:divide-x divide-gray-300 relative">
-            {/* Panel Nauczyciela */}
-            <div className="flex-1 text-center flex flex-col items-center">
-              <div className="mb-4 sm:mb-16 sm:px-10 md:px-20">
-                <h3 className="text-2xl font-semibold text-blue-500">
+            <div className="flex-1 text-center flex flex-col items-center py-4">
+              <div className="mb-4 sm:mb-20 sm:px-4 lg:px-20">
+                <h1 className="text-4xl font-bold text-blue-500">
                   Panel
-                </h3>
-                <h2 className="text-4xl font-bold text-blue-500 mb-4">
+                </h1>
+                <h2 className="text-5xl font-bold text-blue-600 mb-4">
                   Nauczyciela
                 </h2>
-                <p className="text-gray-600 text-md">
+                <p className="text-gray-600 text-md text-pretty">
                   Twórz i przeglądaj swoje klasy oraz zarządzaj dostępem dla rodziców.
                 </p>
               </div>
-              <div className="sm:absolute bottom-0 mb-8 sm:mb-0">
+              <div className="sm:absolute bottom-4 mb-4 sm:mb-0">
                 <ColorfulLinkButton
-                  color="blue"
-                  style={{ padding: "0.5rem 1.5rem"}}
+                  color="primary_blue"
+                  style={{ padding: "0.75rem 2rem", fontSize: "1.2rem" }}
                   to="/teacher/login"
-                  text="Przejdź do logowania"
+                  text="Zaloguj się"
                 />
               </div>
             </div>
 
-            {/* Panel Rodzica */}
-            <div className="flex-1 text-center flex flex-col items-center">
-              <div className="mb-4 sm:mb-16 sm:px-10 md:px-20 mt-4 sm:mt-0">
-                <h2 className="text-2xl font-semibold text-green-500">
+            <div className="flex-1 text-center flex flex-col items-center py-4">
+              <div className="mb-4 sm:mb-20 sm:px-4 lg:px-20 mt-4 sm:mt-0">
+              <h1 className="text-4xl font-bold text-green-500">
                   Panel
-                </h2>
-                <h2 className="text-4xl font-bold text-green-500 mb-4">
+                </h1>
+                <h2 className="text-5xl font-bold text-green-600 mb-4">
                   Rodzica
                 </h2>
-                <p className="text-gray-600 text-md">
+                <p className="text-gray-600 text-md text-pretty">
                   Zarządzaj uprawnieniami i opiekunami oraz przeglądaj historię odbiorów.
                 </p>
               </div>
-              <div className="sm:absolute bottom-0">
+              <div className="sm:absolute bottom-4">
                 <ColorfulLinkButton
-                  color="green"
-                  style={{ padding: "0.5rem 1.5rem" }}
+                  color="primary_green"
+                  style={{ fontSize: "1.2rem", padding: "0.75rem 2rem" }}
                   to="/parent/login"
-                  text="Przejdź do logowania"
+                  text="Zaloguj się"
                 />
               </div>
             </div>
